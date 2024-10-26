f1457e875c2856216fae08233bffc71ae1db382d
Natalia <n.maltseva2@school.nsu.ru>
2024-10-26 15:55:54 +0700

    Решила ещё 3 задачи

--- task4.py
+++ task4.py
@@ -3,3 +3,20 @@
 # Сымитруем данные списком длины N со случайными числами в диапазоне от 0 до 1000
 # Удалите из этого списка все значения, которые на 30 % отличаются
 # от среднего значения списка
+from sympy.core.random import randint
+
+N = int(input())
+m = []
+sum = 0
+for i in range(N):
+    m.append(randint(0, 1000))
+    sum += m[-1]
+sum /= len(m)
+t1, t2 = sum - (sum * 0.3), sum + (sum * 0.3)
+res = []
+for i in m:
+    res.append(i)
+for i in range(len(m)):
+    if(t1 > m[i] or t2 < m[i]):
+        res.remove(m[i])
+print(res)
--- task6.py
+++ task6.py
@@ -4,3 +4,30 @@
 # Пример: ввод N = 6
 # [1,2,3,4,5,6]
 # Вывод: [3,2,1,6,5,4]
+from random import randint
+
+N = int(input())
+m = []
+for i in range(N):
+    m.append(randint(-1000, 1000))
+
+def F1(m):
+    a = []
+    b = []
+    for i in range(N):
+        if (i < N / 2):
+            a.append(m[i])
+        else:
+            b.append(m[i])
+    res = a[::-1] + b[::-1]
+    return res
+
+def F2(m):
+    n = N
+    for i in range((N//2)//2):
+        m[i], m[(n//2) - 1 - i] = m[(n//2) - 1 - i], m[i]
+    for i in range(N//2, N//2 + N//2//2):
+        m[i], m[n - (i - (n // 2)) - 1] = m[n - (i - (n // 2)) - 1], m[i]
+    return m
+
+#F1 и F2 - две разные функции, решающие одну и ту же задачу
